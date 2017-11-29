7f736062eaaa6d12f9febbf4727be9f5ad581df0
Martin Zikmund <martinzikmund@live.com>
2017-11-29 12:16:59 +0100

    Sample for the issue

---
+++ MvvmCrossMultiViewUWPIssue.UWP/Views/FirstView.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+using MvvmCross.Uwp.Views;
+
+namespace MvvmCrossMultiViewUWPIssue.UWP.Views
+{
+    public sealed partial class FirstView : MvxWindowsPage
+    {
+        public FirstView()
+        {
+            this.InitializeComponent();
+        }
+
+private async void CreateNewViewClick(Object sender, RoutedEventArgs e)
+{
+    CoreApplicationView newView = CoreApplication.CreateNewView();
+    int newViewId = 0;
+    await newView.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+    {
+        Frame frame = new Frame();
+        // When the navigation stack isn't restored navigate to the first page,
+        // configuring the new page by passing required information as a navigation
+        // parameter
+        var setup = new Setup(frame);
+        setup.Initialize();
+
+        var start = Mvx.Resolve<IMvxAppStart>();
+        start.Start();
+        Window.Current.Content = frame;
+        // You have to activate the window in order to show it later.
+        Window.Current.Activate();
+
+        newViewId = ApplicationView.GetForCurrentView().Id;
+    });
+    bool viewShown = await ApplicationViewSwitcher.TryShowAsStandaloneAsync(newViewId);
+}
+    }
+}
